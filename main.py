508e8a95b64fcafc1a952f5339b4fac5bead8e4a
w00her <rodon@protonmail.ch>
2023-10-05 01:37:40 +0300

    2/15

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-print('First line')
-print('Second line')
-print('Third line')
-print('')
+result = None
+operand = None
+operator = None
+wait_for_number = True
+
+while True:
+    if wait_for_number:
+        try:
+            operand = input()
+            operand = int(operand)
+        except ValueError:
+            print(f"{operand} is not a number. Try again.")
+            continue
+        wait_for_number = False
+        if result is None:
+            result = operand
+        elif operator == '+':
+            result += operand
+        elif operator == '-':
+            result -= operand
+        elif operator == '/':
+            result /= operand
+        elif operator == '*':
+            result *= operand
+
+    if not wait_for_number:
+        operator = input()
+        if operator == '-' or operator == '+' or operator == '/' or operator == '*':
+            wait_for_number = True
+        elif operator == '=':
+            print(f"Result: {result}")
+            break
+        else:
+            print(f"{operator} is not '+' or '-' or '/' or '*'. Try again")
+            continue
